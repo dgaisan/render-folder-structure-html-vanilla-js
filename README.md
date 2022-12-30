3667e037810c4d4a4bbe906d48f4bae0badd214d
dgaisan <dgaisan@grio.com>
2022-12-30 00:20:36 -0700

    adding view rendering and support for expanding/collapsing of folder.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # render-folder-structure-html-vanilla-js
+This code renders folders/files component using vanilla javascript, DOM APIs, css, html, and font-awesome.
+Input is a tree data structure resembling folder/files structure on a disk.
+It also supports collapsing/expanding of folders when a folder is clicked. 
+
+## Sample folders/files structure
+```
+public
+   index.html
+   favicon.ico
+   robots.txt
+src
+   components
+      timeline
+         index.js
+         index.test.js
+         styles.css
+      user
+         index.js
+         index.test.js
+   App.css
+   App.js
+   App.test.js
+package.json
+README.md
+```
--- script.js
+++ script.js
@@ -1,42 +1,107 @@
 (function run(folders = []) {
     if (!folders.length) { return; }
-
+    
+    const folderIcon = `<i class="fas fa-folder"></i>`;
+    const htmlElementTemplate = 
+        `<div id="{0}" class="{4}" style="position:relative; left:{1}">
+            {5}
+            <span>{2}</span>{3}
+        </div>`;
+    const leftOffsetStep = 10;
     const rootElement = document.querySelector('#root');
-    let currentState = null;   
+    
+    let currentState = null;
+    const idToElementMap = {};
 
+    // initial render
     renderView();
 
     function renderView() {
         const state = getCurrentState();
-        rootElement.innerHTML = `<b onClick="${createClickEvents()}">folders will be displayed here...</b>`;
-        console.table('state:', state);
+        const html = createHtmlFromState(state);
+        
+        rootElement.innerHTML = `<div id="files">${html}</div>`;
+        createClickEvents();
     }
 
     function createClickEvents() {
-        return "eval(alert('handle click...'))";
+        const directories = document.querySelectorAll('#files .directory,.file');
+        directories.forEach((dir) => {
+            dir.addEventListener('click', setClickEvent);
+        });
+        console.log(directories);
+    }
+
+    function setClickEvent(event) {
+        event.stopPropagation();
+
+        if (event.currentTarget && event.currentTarget.classList.contains('directory')) {
+            const clickedElementId = event.currentTarget.id;
+            toggleFolderExpandState(clickedElementId);
+            renderView();
+        }
+    }
+
+    function toggleFolderExpandState(folderId) {
+        idToElementMap[folderId].isExpanded = !idToElementMap[folderId].isExpanded;
     }
 
     function getCurrentState() {
         if (currentState) { return currentState; }
-        return folders.reduce((files, currentFile) => {
-            assignLevelsAndExpandStatusToFiles(currentFile, 1);
+        currentState = folders.reduce((files, currentFile) => {
+            assignFileStateProps(currentFile, 1);
             console.log(currentFile);
             return [...files, currentFile];
-        }, [])
+        }, []);
+        return currentState;
     }
 
-    function assignLevelsAndExpandStatusToFiles(currentFolder, level) {
+    function assignFileStateProps(currentFolder, level) {
         currentFolder.level = level;
         currentFolder.isExpanded = true;
-        UUID
+        currentFolder.id = crypto.randomUUID();
+        idToElementMap[currentFolder.id] = currentFolder;
+
         if (currentFolder.children && currentFolder.children.length) {
-            currentFolder.children.forEach(f => { assignLevelsAndExpandStatusToFiles(f, level + 1); })
+            currentFolder.children.forEach(f => { assignFileStateProps(f, level + 1); })
         }
+    }
+
+    function createHtmlFromState(state) {
+        return state.reduce((html, currentFile) => {
+            const isDirectory = currentFile.children && currentFile.children.length;
+            let children = "";
+            
+            if (isDirectory && currentFile.isExpanded) {
+                children = createHtmlFromState(currentFile.children);
+            }
+
+            return html + 
+                format(
+                    htmlElementTemplate, 
+                    currentFile.id, 
+                    currentFile.level * leftOffsetStep, 
+                    currentFile.name, 
+                    children,
+                    isDirectory ? "directory" : "file",
+                    isDirectory ? folderIcon : "");
+        }, '');
+    }
+
+    // string formating function: 
+    // format("Formatted {0}, formatted {1}", "one", "two")
+    function format(str, ...args) {
+        return str.replace(/{(\d+)}/g, function(match, number) { 
+          return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+          ;
+        });
     }
 
 })(createTestFoldersTree());
 
-
+// Test cases
 function createTestFoldersTree() {
     return [
         {
