7f6d37091b3a1ed77d43a600b7e7e7b75c21230a
dgaisan <dgaisan@grio.com>
2022-12-29 21:19:17 -0700

    Adding state properties to each file/folder

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 (function run(folders = []) {
     if (!folders.length) { return; }
 
-    const rootElement = document.querySelector('#root');    
+    const rootElement = document.querySelector('#root');
+    let currentState = null;   
 
     renderView();
 
     function renderView() {
+        const state = getCurrentState();
         rootElement.innerHTML = `<b onClick="${createClickEvents()}">folders will be displayed here...</b>`;
+        console.table('state:', state);
     }
 
     function createClickEvents() {
         return "eval(alert('handle click...'))";
     }
+
+    function getCurrentState() {
+        if (currentState) { return currentState; }
+        return folders.reduce((files, currentFile) => {
+            assignLevelsAndExpandStatusToFiles(currentFile, 1);
+            console.log(currentFile);
+            return [...files, currentFile];
+        }, [])
+    }
+
+    function assignLevelsAndExpandStatusToFiles(currentFolder, level) {
+        currentFolder.level = level;
+        currentFolder.isExpanded = true;
+        UUID
+        if (currentFolder.children && currentFolder.children.length) {
+            currentFolder.children.forEach(f => { assignLevelsAndExpandStatusToFiles(f, level + 1); })
+        }
+    }
+
 })(createTestFoldersTree());
 
 
